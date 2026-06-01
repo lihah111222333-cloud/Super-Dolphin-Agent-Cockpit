4f2d244525a0e2ee17b34198df24838320fafd42
Toolbridge Test <xiaoxiaotest9527@gmail.com>
2026-06-01 22:21:54 +0800

    feat: 新增 observability 内存索引与查询服务

---
+++ internal/platform/observability/query.go
@@ -0,0 +1,23 @@
+package observability
+
+type QuerySource string
+
+const (
+	QuerySourceMemory    QuerySource = "memory"
+	QuerySourceJSONLTail QuerySource = "jsonl_tail"
+	QuerySourceMixed     QuerySource = "mixed"
+)
+
+type Query struct {
+	TraceID     string
+	ThreadID    string
+	Slow        bool
+	Errors      bool
+	Limit       int
+	IncludeTail bool
+}
+
+type QueryResult struct {
+	Source QuerySource
+	Events []TraceEvent
+}
